ba56ba6cbf87a2991fac69e1f4f4dfe8118c1bd7
siva857m <siva857m@gmail.com>
2026-07-05 23:26:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ async def effective_config(request: Request):
         "api_key": "default-secret-000"
     }
 
+    # 2. YAML layer
+    if os.path.exists("config.development.yaml"):
+        with open("config.development.yaml", "r") as f:
+            yaml_config = yaml.safe_load(f)
+            config.update(yaml_config)
 
     # 3. .env layer
     if os.getenv("APP_PORT"):
@@ -37,14 +42,8 @@ async def effective_config(request: Request):
         config["workers"] = int(os.getenv("NUM_WORKERS"))
     if os.getenv("APP_API_KEY"):
         config["api_key"] = os.getenv("APP_API_KEY")
-     # 2. YAML layer
-    
-    if os.path.exists("config.development.yaml"):
-        with open("config.development.yaml", "r") as f:
-            yaml_config = yaml.safe_load(f)
-            config.update(yaml_config)
 
-    # 4. OS env vars (APP_* prefix)
+    # 4. OS env vars
     if os.getenv("APP_WORKERS"):
         config["workers"] = int(os.getenv("APP_WORKERS"))
 
@@ -68,3 +67,4 @@ async def effective_config(request: Request):
     masked["api_key"] = "****"
 
     return JSONResponse(masked)
+
